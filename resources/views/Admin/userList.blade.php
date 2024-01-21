24ebc6b5371efb09a136c950023ecdd86f4a9a28
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-01-21 21:04:01 +0800

    Admin Controller

--- resources/views/Admin/userList.blade.php
+++ resources/views/Admin/userList.blade.php
@@ -585,11 +585,11 @@
                             <div class="modal-dialog modal-dialog-centered">
                               <div class="modal-content">
                                 <div class="modal-header">
-                                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                  <h5 class="modal-title" id="exampleModalLabel">Profile View</h5>
                                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
-                                  MOdal 1
+                                  
                                 </div>
                                 <div class="modal-footer">
                                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
