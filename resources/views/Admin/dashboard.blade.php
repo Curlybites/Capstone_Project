8af18970566e091258aac6945b3bfee69eb35a69
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-01-21 19:08:07 +0800

    Update dashboard.blade.php

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -14,10 +14,14 @@
 
           </a>
 
+          <a href=""> HATDOG</a>
+
           <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
             <i class="bx bx-chevron-left bx-sm align-middle"></i>
           </a>
         </div>
+
+        
 
         <div class="menu-inner-shadow"></div>
 
