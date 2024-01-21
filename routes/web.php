5e31f1b7a1aa161e9c83294a5528452a1cc3889a
Alphy <alphy.balatucan16@gmail.com>
2024-01-21 22:36:38 +0800

    Updated

--- routes/web.php
+++ routes/web.php
@@ -22,29 +22,28 @@ use Illuminate\Support\Facades\Auth;
 // });
 
 
-Route::controller(PageController::class)->group(function(){
-    Route::get('/','loginPage');
-    Route::get('/Admin/Login','AdminLogin');
-    Route::get('/District/Login','districtPage');
-    Route::get('/Program_Manager/Login','pmPage');
-    Route::get('/Health_Department/Login','hdPage');
-    Route::get('/Health_Center/Login','hcPage');
-    Route::get('/Project_Manager/Dashboard','pmDashboard');
-    Route::get('/Admin/Dashboard','adminDashboard');
-   
+Route::controller(PageController::class)->group(function () {
+    Route::get('/', 'loginPage');
+    Route::get('/Admin/Login', 'AdminLogin');
+    Route::get('/District/Login', 'districtPage');
+    Route::get('/Program_Manager/Login', 'pmPage');
+    Route::get('/Health_Department/Login', 'hdPage');
+    Route::get('/Health_Center/Login', 'hcPage');
+    Route::get('/Project_Manager/Dashboard', 'pmDashboard');
+    Route::get('/Admin/Dashboard', 'adminDashboard');
 });
 
-Route::controller(UserController::class)->group(function(){
-    Route::get('/Admin/User_List','userlistPage');
-    Route::post('/Admin/Store','store');
-    Route::get('/Admin/Add_User','createUserPage');
-    Route::get('/Admin/User_List','userList');
-    Route::post('/Admin/Login/Process','LoginProcess');
+Route::controller(UserController::class)->group(function () {
+    Route::get('/Admin/User_List', 'userlistPage');
+    Route::post('/Admin/Store', 'store');
+    Route::get('/Admin/Add_User', 'createUserPage');
+    Route::get('/Admin/User_List', 'userList');
+    Route::post('/Admin/Login/Process', 'LoginProcess');
 });
 
 
 // Admin route
 
-Route::controller(AdminController::class)->group(function(){
+Route::controller(AdminController::class)->group(function () {
     Route::get('/Admin/District_List', 'districtListpage');
 });
