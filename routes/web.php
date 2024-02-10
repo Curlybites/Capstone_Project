eff8a72b611cab562e34e633fc7b7e469e789a75
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-02-10 18:33:38 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -29,82 +29,7 @@ use Illuminate\Support\Facades\Auth;
 // Route For Login
 Route::get('/',[PageController::class,'AdminLogin'])->name('login');
 
-//Route for User Controller 
-Route::controller(UserController::class)->group(function () {
-    // Route::get('/Admin/User_List', 'userlistPage');
-    // Route::post('/Admin/Store', 'store');
-    // Route::get('/Admin/Add_User', 'createUserPage')->middleware('auth');
-    // Route::get('/Admin/User_List', 'userList')->middleware('auth');
-    // Route::get('/Admin/User_List/$id',)->middleware('auth');
-    Route::post('/Login/Process', 'LoginProcess');
-    Route::post('/Logout', 'logout');
-    Route::get('/change-password', 'changePassword')->name('changePassword');
-    Route::post('/change-password', 'ChangePasswordSave')->name('changePass');
-});
 
-
-
-<<<<<<< Updated upstream
-<<<<<<<<< Temporary merge branch 1
-=======
-// Route For Admin
-Route::middleware(['auth', 'admin'])->group(function () {
-
-    Route::get('/Admin/Dashboard', [PageController::class,'adminDashboard']);
-
-    Route::post('/Admin/Store', [UserController::class,'store']);
-    Route::get('/Admin/Add_User', [UserController::class,'createUserPage']);
-    Route::get('/Admin/User_List', [UserController::class,'userList']);
-    Route::get('/Admin/User_List/$id', [UserController::class,'']);
-    
-    // Route::post('/Logout', [UserController::class,'logout']);
-    // Route::get('/change-password', [UserController::class,'changePassword']);
-    // Route::get('/change-password', [UserController::class,'changePass']);
-}
->>>>>>> Stashed changes
-Route::controller(AdminController::class)->group(function () {
-    Route::post('/Admin/District_Store', 'districtStore');
-    Route::post('/Admin/Barangay_Store', 'barangayStore');
-    Route::post('/Admin/Health_Center_Store', 'healthcenterStore');
-    Route::post('/Admin/Program_Store', 'programStore');
-    Route::get('/Admin/District_List', 'districtListpage')->middleware('auth');
-    Route::put('/Admin/District_List/{id}', 'updateDistrict');
-    Route::get('/Admin/Account_Profile', 'profile')->middleware('auth');
-    Route::get('/Admin/Account_Change_Password', 'profileChange')->middleware('auth');
-    Route::get('/Admin/Barangay_List', 'barangayPage');
-    Route::put('/Admin/Barangay_List/{id}', 'updateBarangay');
-    Route::get('/Admin/Health_Center_List', 'Healthcenterpage');
-    Route::put('/Admin/Health_Center_List/{id}', 'updatehc');
-    Route::get('/Admin/Program_List', 'Programpage');
-    Route::put('Admin/Program_List/{id}', 'programUpdate');
-});
-
-
-// Health Department Route
-Route::controller(HdController::class)->group(function () {
-    Route::get('/Health_Department/Item_List', 'hdInventory')->middleware('auth');
-    Route::get('/Health_Department/Allocation_List', 'hdAllocation')->middleware('auth');
-    Route::get('/Health_Department/Allocation_Process', 'hdAllocationProcess')->middleware('auth');
-    Route::get('/Health_Department/Allocation_View_{id}', 'hdAllocationView')->middleware('auth');
-    Route::get('/Health_Department/Allocation_Edit_{id}', 'hdAllocationEdit')->middleware('auth');
-    Route::get('/Health_Department/Profile', 'hdAccount')->middleware('auth');
-    Route::get('/Health_Department/Purchase_Order_View', 'hdPurchaseOrderView')->middleware('auth');
-    Route::get('/Health_Department/Purchase_Order_List', 'hdPurchaseOrderList')->middleware('auth');
-    Route::get('/Health_Department/Profile_Change', 'hdAccountChange')->middleware('auth');
-
-    Route::post('/Health_Department/Allocation_Process', 'hdAllocationtoProg')->name('allocate')->middleware('auth');
-    Route::put('/Health_Department/Allocation_Edit_{id}', 'hdAllocationProgUpdate')->name('updateallocation')->middleware('auth');
-    Route::delete('/Health_Department/Allocation_List{id}', 'hdAllocationProgDelete')->name('deleteallocation');
-});
-
-
-// Supplier Route
-Route::controller(SupplierController::class)->group(function () {
-    Route::get('/Supplier/Item_List', 'itemList')->middleware('auth');
-    Route::get('/Supplier/Account_Profile', 'profileSupplier')->middleware('auth');
-    Route::get('/Supplier/Account_Change_Password', 'profileChangeSupplier')->middleware('auth');
-    Route::get('/Supplier/PPMP_List', 'ppmp')->middleware('auth');
-=========
 // Route For Admin
 Route::middleware(['auth', 'admin'])->group(function () {
 
@@ -175,8 +100,8 @@ Route::middleware(['auth', 'health_department'])->group(function () {
     Route::post('/Health_Department/Allocation_Process', [HdController::class,'hdAllocationtoProg'])->name('allocate');
 
 
-    Route::put('/Health_Department/Allocation_Edit_{id}', 'hdAllocationProgUpdate')->name('updateallocation')->middleware('auth');
-    Route::delete('/Health_Department/Allocation_List{id}', 'hdAllocationProgDelete')->name('deleteallocation');
+    Route::put('/Health_Department/Allocation_Edit_{id}', [HdController::class, 'hdAllocationProgUpdate'])->name('updateallocation');
+    Route::delete('/Health_Department/Allocation_List{id}', [HdController::class, 'hdAllocationProgDelete'])->name('deleteallocation');
 
 });
 
@@ -282,4 +207,4 @@ Route::middleware(['auth', 'supplier'])->group(function () {
 //     // Route::get('/Supplier/Account_Change_Password', 'profileChangeSupplier')->middleware('auth');
 //     // Route::get('/Supplier/PPMP_List', 'ppmp')->middleware('auth');
 // });
->>>>>>>>> Temporary merge branch 2
+
