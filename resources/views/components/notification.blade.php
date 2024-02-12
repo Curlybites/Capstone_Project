21f29fe33f8944ff6979c57b7843e3c1f44dbe76
Anthony Esquilona <91820043+Curlybites@users.noreply.github.com>
2024-02-12 23:36:55 +0800

    Update notification.blade.php

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,4 +1,4 @@
-<div class="bs-toast toast fade show position-fixed bottom-0 end-0 m-3 bg-primary " role="alert" aria-live="assertive" aria-atomic="true">
+<div class="bs-toast toast fade show position-fixed bottom-0 end-0 m-3  " role="alert" aria-live="assertive" aria-atomic="true">
                 <div class="toast-header">
                 <i class='bx bx-check d-block w-px-20 h-auto rounded me-2' ></i>
                 {{-- <i class='bx bxs-bell  d-block w-px-20 h-auto rounded me-2'></i> --}}
